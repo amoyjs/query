0a41406d89150532bd4b2ba2395a2f19319ad443
大板栗 <576839360@qq.com>
2019-08-01 19:14:11 +0800

    📝 integrate package `@amoy/query-event`

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,12 @@
 import { extend } from '../globals'
 import { createQuery } from './createQuery'
+import queryEvent from '@amoy/query-event'
 
 // @ts-ignore
 export const query = (stage: any) => window.$ = createQuery(stage, query)
 
 query.extend = extend
+
+query.extend([
+    queryEvent,
+])
--- src/globals/extend.ts
+++ src/globals/extend.ts
@@ -1,3 +1,9 @@
-export function extend(object: any = {}) {
-    Object.assign(this.prototype, object)
+export function extend(target: any = []) {
+    if (Array.isArray(target)) {
+        target.map((item) => extend.bind(this)(item))
+    } else if (typeof target === 'object') {
+        Object.assign(this.prototype, target)
+    } else {
+        console.error(`query extension's type must be "object" or "object[]"`)
+    }
 }
